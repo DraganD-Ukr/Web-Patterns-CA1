1bea1684dcc33792049ebee95ea1aff5304ad92c
Aloysius <aloysiuspacheco2003@gmail.com>
2024-10-08 10:47:32 +0100

    Created Song DTO

--- src/main/java/Business/Song.java
+++ src/main/java/Business/Song.java
@@ -1,4 +1,36 @@
 package Business;
 
+
+import lombok.*;
+
+import java.time.LocalTime;
+
+
+
+
+/**
+ * Song DTO used to hold song data from database
+ *
+ * @author Aloysius Wilfred Pacheco D00253302
+ */
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@EqualsAndHashCode
+
 public class Song {
+    @NonNull
+    private int songID;
+    @NonNull
+    private String title;
+    @NonNull
+    private int albumID;
+    @NonNull
+    private int artistID;
+    private LocalTime length;
+    private int ratingCount;
+    private double averageRating;
+    private int ratingsSum;
+
 }
