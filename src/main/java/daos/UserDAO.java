8aa38ec074ba3fa1ef908b55eb83e6080d1a0217
Joart <joartmahilaga69@gmail.com>
2024-10-16 15:32:58 +0100

    feat: added encryption for password encrypt using Bcrypt For SHA 256 changes: completed 3/4 of total implementations

--- src/main/java/daos/UserDAO.java
+++ src/main/java/daos/UserDAO.java
@@ -1,7 +1,16 @@
 package daos;
+import Business.User;
+
+import java.util.List;
 
 public interface userDAO {
 
+    boolean addUser(User user);
 
-    
+    String getPasswordByUserName(String userName);
+
+    User getUserByID(String userName);
+
+    boolean deleteUser(User user);
+
 }
--- src/main/java/daos/UserDAOimpl.java
+++ src/main/java/daos/UserDAOimpl.java
@@ -1,8 +1,123 @@
 package daos;
 
-public class userDAOimpl {
+import Business.User;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDateTime;
+
+public class userDAOimpl extends Dao implements userDAO {
+
+    public userDAOimpl(String databaseName) {
+        super(databaseName);
+    }
+
+    @Override
+    public boolean addUser(User user) {
+
+        if (user == null || user.getUserName() == null || user.getPassword() == null) {
+            return false;
+        }
+
+        Connection conn = super.getConnection();
+
+        try (PreparedStatement ps = conn.prepareStatement("INSERT INTO Users (firstName, lastName, userName, password) VALUES (?, ?, ?, ?)")) {
+
+            ps.setString(1, user.getFirstName());
+            ps.setString(2, user.getLastName());
+            ps.setString(3, user.getUserName());
+            ps.setString(4, user.getPassword());
+
+            int rowsAffected = ps.executeUpdate();
+
+            return rowsAffected > 0;
+
+        } catch (SQLException e) {
+            System.out.println(LocalDateTime.now() + ": An SQLException occurred while adding a user \n Error:" + e.getMessage());
+            e.printStackTrace();
+
+            return false;
 
 
+        } catch (NullPointerException e) {
+            System.out.println(LocalDateTime.now() + ": A NullPointerException occurred \n Error: " + e.getMessage());
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    @Override
+    public String getPasswordByUserName(String userName) {
+        return "";
+    }
 
 
+    @Override
+    public User getUserByID(String userName) {
+        User user = null;
+
+        if (userName == null) {
+            return null;
+        }
+
+        try (Connection con = getConnection();
+
+             PreparedStatement ps = con.prepareStatement("SELECT * FROM Users WHERE userName = ?")) {
+
+            ps.setString(1, userName);
+
+            try (ResultSet rs = ps.executeQuery()) {
+                if (rs.next()) {
+                    user = new User(
+                            rs.getString("firstName"),
+                            rs.getString("lastName"),
+                            rs.getString("userName"),
+                            rs.getString("password"),
+                            rs.getInt("userID")
+                    );
+                }
+            }
+        } catch (SQLException e) {
+            System.out.println(LocalDateTime.now() + ": An SQLException occurred in the getUserByID() method.\nError: " + e.getMessage());
+            e.printStackTrace();
+
+
+        } catch (NullPointerException e) {
+            System.out.println(LocalDateTime.now() + ": A NullPointerException occurred in the getUserByID() method.\nError: " + e.getMessage());
+            e.printStackTrace();
+        }
+
+        return user;
+    }
+
+    @Override
+    public boolean deleteUser(User user) {
+
+        if (user == null || user.getUserID() <= 0) {
+            return false;
+        }
+
+        try (Connection con = super.getConnection();
+
+             PreparedStatement ps = con.prepareStatement("DELETE FROM Users WHERE userID = ?")) {
+
+            ps.setInt(1, user.getUserID());
+
+            int rowsAffected = ps.executeUpdate();
+
+            return rowsAffected > 0;
+
+        } catch (SQLException e) {
+            System.out.println(LocalDateTime.now() + ": An SQLException occurred while deleting a user.\nError: " + e.getMessage());
+            e.printStackTrace();
+            return false;
+
+        } catch (NullPointerException e) {
+            System.out.println(LocalDateTime.now() + ": A NullPointerException occurred while deleting a user.\nError: " + e.getMessage());
+            e.printStackTrace();
+            return false;
+        }
+    }
 }
