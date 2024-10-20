b2b957a755e18ea89dd98b7d77bdbc38040d985d
Dmytro Drahan <dmitrijrolkin@gmail.com>
2024-10-20 18:50:08 +0100

    Added playlist DTO

--- src/main/java/Business/Playlist.java
+++ src/main/java/Business/Playlist.java
@@ -1,4 +1,33 @@
 package Business;
 
+import lombok.*;
+
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+@Builder
 public class Playlist {
+
+    @NonNull
+    private int playlistId;
+    @NonNull
+    private int userId;
+    @NonNull
+    private String name;
+    @NonNull
+    private boolean isPublic;
+
+    /**
+     * Constructor for creating a playlist object
+     * @param userId The id of the user who created the playlist
+     * @param name The name of the playlist
+     * @param isPublic Whether the playlist is public or not
+     */
+    public Playlist(int userId, String name, boolean isPublic) {
+        this.userId = userId;
+        this.name = name;
+        this.isPublic = isPublic;
+    }
+
 }
