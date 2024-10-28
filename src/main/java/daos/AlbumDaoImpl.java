cb156644e67b0cda39a6c6ab9b08d06aac04d441
Dmytro Drahan <dmitrijrolkin@gmail.com>
2024-10-28 14:00:36 +0000

    Refactored Album DAO impl to use try-with-resources

--- src/main/java/daos/AlbumDaoImpl.java
+++ src/main/java/daos/AlbumDaoImpl.java
@@ -20,38 +20,38 @@ public class AlbumDaoImpl extends Dao implements AlbumDAO{
     @Override
     public List<Album> getAllAlbumsByArtistId(int artistId) {
 
-        Connection con = null;
-        PreparedStatement ps = null;
-        ResultSet rs = null;
+
 
         Album album;
         List<Album> result = new ArrayList<>();
+        String query = "SELECT * FROM Albums WHERE artistID = ? ORDER BY albumID";
+
+        try (Connection con = getConnection();
+             PreparedStatement ps = con.prepareStatement(query)) {
 
 
-        try {
-            con = getConnection();
-
-            String query = "SELECT * FROM Albums WHERE artistID = ? ORDER BY albumID";
-            ps = con.prepareStatement(query);
             ps.setInt(1, artistId);
-            rs = ps.executeQuery();
 
-            while (rs.next()) {
+            try(ResultSet rs = ps.executeQuery()) {
+                while (rs.next()) {
 //                Using builder for easier object creation
-                album = Album.builder()
-                        // Get the properties of an artist from the resultset
-                        .albumId(rs.getInt("artistId"))
-                        .title(rs.getString("title"))
-                        .artistId(rs.getInt("artistId"))
-                        .releaseDate(rs.getDate("releaseDate"))
-                        .build();
+                    album = Album.builder()
+                            // Get the properties of an artist from the resultset
+                            .albumId(rs.getInt("artistId"))
+                            .title(rs.getString("title"))
+                            .artistId(rs.getInt("artistId"))
+                            .releaseDate(rs.getDate("releaseDate"))
+                            .build();
 //                Add the artist to the result list
-                result.add(album);
+                    result.add(album);
+                }
+            } catch (SQLException e) {
+                System.out.println("Exception occurred in the getArtistById() method: " + e.getMessage());
             }
+
+
         } catch (SQLException e) {
             System.out.println("Exception occurred in the getArtistById() method: " + e.getMessage());
-        } finally {
-            closeResources(rs, ps, con);
         }
 
         return result;
@@ -60,42 +60,44 @@ public class AlbumDaoImpl extends Dao implements AlbumDAO{
     @Override
     public List<Album> getAllAlbumsWhereArtistNameLike(String artistName) {
 
-        Connection con = null;
-        PreparedStatement ps = null;
-        ResultSet rs = null;
+
 
         Album album;
         List<Album> result = new ArrayList<>();
-
-
-        try {
-            con = getConnection();
-
-            String query = """
+        String query = """
                     SELECT Albums.*\s
                     FROM Albums
                     JOIN Artists ON Albums.artistID = Artists.artistID
                     WHERE Artists.name LIKE ?""";
-            ps = con.prepareStatement(query);
-            ps.setString(1, "%" + artistName + "%");
-            rs = ps.executeQuery();
 
-            while (rs.next()) {
+        try (Connection con = getConnection();
+             PreparedStatement ps = con.prepareStatement(query)
+        ) {
+
+            ps.setString(1, "%" + artistName + "%");
+
+            try (ResultSet rs = ps.executeQuery()) {
+
+                while (rs.next()) {
 //                Using builder for easier object creation
-                album = Album.builder()
-                        // Get the properties of an artist from the resultset
-                        .albumId(rs.getInt("artistId"))
-                        .title(rs.getString("title"))
-                        .artistId(rs.getInt("artistId"))
-                        .releaseDate(rs.getDate("releaseDate"))
-                        .build();
+                    album = Album.builder()
+                            // Get the properties of an artist from the resultset
+                            .albumId(rs.getInt("artistId"))
+                            .title(rs.getString("title"))
+                            .artistId(rs.getInt("artistId"))
+                            .releaseDate(rs.getDate("releaseDate"))
+                            .build();
 //                Add the artist to the result list
-                result.add(album);
+                    result.add(album);
+                }
+
+            } catch (SQLException e) {
+                System.out.println("Exception occurred in the getArtistById() method: " + e.getMessage());
             }
+
+
         } catch (SQLException e) {
             System.out.println("Exception occurred in the getArtistById() method: " + e.getMessage());
-        } finally {
-            closeResources(rs, ps, con);
         }
 
         return result;
@@ -104,42 +106,45 @@ public class AlbumDaoImpl extends Dao implements AlbumDAO{
     @Override
     public List<Album> getAllAlbumsByArtistName(String artistName) {
 
-        Connection con = null;
-        PreparedStatement ps = null;
-        ResultSet rs = null;
+
 
         Album album;
         List<Album> result = new ArrayList<>();
 
-
-        try {
-            con = getConnection();
-
-            String query = """
+        String query = """
                     SELECT Albums.*\s
                     FROM Albums
                     JOIN Artists ON Albums.artistID = Artists.artistID
                     WHERE Artists.name = ?""";
-            ps = con.prepareStatement(query);
-            ps.setString(1, artistName);
-            rs = ps.executeQuery();
 
-            while (rs.next()) {
+        try (Connection con = getConnection();
+                PreparedStatement ps = con.prepareStatement(query);
+            ) {
+
+            ps.setString(1, artistName);
+
+            try (ResultSet rs = ps.executeQuery()) {
+
+                while (rs.next()) {
 //                Using builder for easier object creation
-                album = Album.builder()
-                        // Get the properties of an artist from the resultset
-                        .albumId(rs.getInt("artistId"))
-                        .title(rs.getString("title"))
-                        .artistId(rs.getInt("artistId"))
-                        .releaseDate(rs.getDate("releaseDate"))
-                        .build();
+                    album = Album.builder()
+                            // Get the properties of an artist from the resultset
+                            .albumId(rs.getInt("artistId"))
+                            .title(rs.getString("title"))
+                            .artistId(rs.getInt("artistId"))
+                            .releaseDate(rs.getDate("releaseDate"))
+                            .build();
 //                Add the artist to the result list
-                result.add(album);
+                    result.add(album);
+                }
+
+            } catch (SQLException e) {
+                System.out.println("Exception occurred in the getArtistById() method: " + e.getMessage());
             }
+
+
         } catch (SQLException e) {
             System.out.println("Exception occurred in the getArtistById() method: " + e.getMessage());
-        } finally {
-            closeResources(rs, ps, con);
         }
 
         return result;
