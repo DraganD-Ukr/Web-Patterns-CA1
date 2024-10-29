b450dee6bd420085a12f91a5272586bbfce43c8c
Joart <joartmahilaga69@gmail.com>
2024-10-29 16:13:18 +0000

    feat: initial application branch commit

---
+++ src/main/java/Application/MusicPlaylistApplication.java
@@ -0,0 +1,31 @@
+package Application;
+
+public class MusicPlaylistApplication {
+
+// create the menu first
+
+
+// do functions one by one
+
+
+    //use encryption from utils package for password hash and salt
+
+
+    //register                       //use regex
+        //validate first name
+        //validate last name
+        //validate username
+        //validate password based on username
+        //re enter password
+        //validate credit card number
+
+
+    //login
+        //validate username
+        //validate password
+        //check if username and password match
+        //if match, login
+        //if not, display error message
+
+
+}
